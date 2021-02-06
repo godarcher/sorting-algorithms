ce9801885d4fee63f95eda15ab26739ab2b339e5
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-07 00:22:34 +0100

    added insertion sort documentation in comments

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,8 +1,23 @@
 
 #! This program uses insertion sort to sort an unserted array and prints it to console
 
+"""
+? Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time. 
+? It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.
+? However, insertion sort provides several advantages:
+    * Simple implementation: Jon Bentley shows a three-line C version, and a five-line optimized version[1]
+    * Efficient for (quite) small data sets, much like other quadratic sorting algorithms
+    * More efficient in practice than most other simple quadratic (i.e., O(n2)) algorithms such as selection sort or bubble sort
+    * Adaptive, i.e., efficient for data sets that are already substantially sorted: the time complexity is O(kn) when each element in the input is no more than k places away from its sorted position
+    * Stable; i.e., does not change the relative order of elements with equal keys
+    * In-place; i.e., only requires a constant amount O(1) of additional memory space
+    * Online; i.e., can sort a list as it receives it
+"""
+
 # * This is the main function for insertionsort
 # It takes an array as input and manipules the array for sorted output
+
+
 def insertion_Sort(input_array):
 
     # For entire input array
