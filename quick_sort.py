d79378963120e8e008b87954fa8005b2fcd37aab
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 00:03:42 +0100

    implemented actual quicksort

--- quick_sort.py
+++ quick_sort.py
@@ -14,3 +14,59 @@
 ? Mathematical analysis of quicksort shows that, on average, the algorithm takes O(n log n) comparisons to sort n items. 
 ? In the worst case, it makes O(n2) comparisons, though this behavior is rare.
 """
+
+
+def partition_array(input_array, low, high):
+    # ? The partition functions takes the last element of an array as pivot.
+    # * It then places this pivot at the correct positions and uses it to sort the remaining data left and right of it.
+
+    # Smaller is left of low, so it should be a smaller element
+    smaller = (low-1)
+
+    # The pivot is the high of the input array (last element)
+    pivot = input_array[high]
+
+    # For every element between low and high
+    for element in range(low, high):
+
+        # If current element is smaller than the pivot
+        if input_array[element] < pivot:
+
+            # We increment the smaller element by 1
+            smaller += 1
+
+            # We swap
+            input_array[smaller], input_array[element] = input_array[element], input_array[smaller]
+
+    # After all the computations, we adjust accordingly
+    input_array[smaller +
+                1], input_array[high] = input_array[high], input_array[smaller+1]
+
+    # We return the value
+    return (smaller+1)
+
+
+def quick_Sort(input_array, low, high):
+    # ? This function uses recursive calls with smaller arrays from partitions
+    # * it continues till it has recursively quicksorted the array
+
+    # If high is higher then low (logical condition)
+    if low < high:
+
+        # input_array[pi] is now rightly sorted
+        partition_index = partition_array(input_array, low, high)
+
+        # Recursively sort the left part of the array (pivot - 1)
+        quick_Sort(input_array, low, partition_index - 1)
+
+        # Recursively sort the right part of the array (pivot + 1)
+        quick_Sort(input_array, partition_index + 1, high)
+
+
+# Driver code
+arr = [10, 7, 8, 9, 1, 5]
+n = len(arr)
+quick_Sort(arr, 0, n-1)
+print("Sorted array is:")
+for i in range(n):
+    print("%d" % arr[i])
