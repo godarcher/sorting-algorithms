551bfcac2436e7c42cd82396919882b2bb58ba8a
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 00:50:12 +0100

    started implementing merge sort

--- merge_sort.py
+++ merge_sort.py
@@ -2,9 +2,9 @@
 #! This program uses merge sort to sort an unserted array and prints it to console
 
 """
-? In computer science, merge sort (also commonly spelled mergesort) is an efficient, general-purpose, comparison-based sorting algorithm. 
-? Most implementations produce a stable sort, which means that the order of equal elements is the same in the input and output. 
-? Merge sort is a divide and conquer algorithm that was invented by John von Neumann in 1945.[2] 
+? In computer science, merge sort (also commonly spelled mergesort) is an efficient, general-purpose, comparison-based sorting algorithm.
+? Most implementations produce a stable sort, which means that the order of equal elements is the same in the input and output.
+? Merge sort is a divide and conquer algorithm that was invented by John von Neumann in 1945.[2]
 ? A detailed description and analysis of bottom-up merge sort appeared in a report by Goldstine and von Neumann as early as 1948.[3]
 
 ? Conceptually, a merge sort works as follows:
@@ -12,42 +12,46 @@
 * Repeatedly merge sublists to produce new sorted sublists until there is only one sublist remaining. This will be the sorted list.
 """
 
-# Function
 
+def mergeSort(input_array):
+    # ? The mergesort function sorts using dividing and merging sublists.
+    # * The input it takes is an input_array, which normally is unsorted.
 
-def mergeSort(arr):
-    if len(arr) > 1:
+    # If the amount of elements inside the array is more then 1 (otherwise already sorted)
+    if len(input_array) > 1:
 
-        # Finding the middle of the array
-        r = len(arr)//2
+        # We take the middle index by dividing the length by 2
+        middle = len(input_array) // 2
 
-        # Dividing array into two halves
-        leftArray = arr[:r]
-        rightArray = arr[r:]
+        # The left part of the array is the input array from start till middle (using substring method)
+        left_side = input_array[:middle]
+
+        # The right part of the array is the input array from middle till end (using substring method)
+        right_side = input_array[middle:]
 
         # Calling mergesort function on subparts of array
-        mergeSort(leftArray)
-        mergeSort(rightArray)
+        mergeSort(left_side)
+        mergeSortright_side)
 
-        i = j = k = 0
+        i=j=k=0
 
         # Copying data to temp arrays leftArray[] and rightArray[]
-        while i < len(leftArray) and j < len(rightArray):
-            if leftArray[i] < rightArray[j]:
-                arr[k] = leftArray[i]
+        while i < len(left_side) and j < len(right_side):
+            if left_side[i] < right_side[j]:
+                input_array[k]=left_side[i]
                 i += 1
             else:
-                arr[k] = rightArray[j]
+                input_array[k]=right_side[j]
                 j += 1
             k += 1
 
-        while i < len(leftArray):
-            arr[k] = leftArray[i]
+        while i < len(left_side):
+            input_array[k]=left_side[i]
             i += 1
             k += 1
 
-        while j < len(rightArray):
-            arr[k] = rightArray[j]
+        while j < len(right_side):
+            input_array[k]=right_side[j]
             j += 1
             k += 1
 
@@ -56,13 +60,13 @@ def mergeSort(arr):
 
 def display(arr):
     for i in range(len(arr)):
-        print(arr[i], end=" ")
+        print(arr[i], end = " ")
     print()
 
 
 # driver code
 if __name__ == '__main__':
-    arr = [6, 5, 12, 10, 9, 1]
+    arr=[6, 5, 12, 10, 9, 1]
     print("Original array")
     display(arr)
     mergeSort(arr)
