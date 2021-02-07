b0d1c780fb509dd6ea45cdcc108aebf93212dfa9
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 00:29:42 +0100

    added merge sort documentation

---
+++ merge_sort.py
@@ -0,0 +1,13 @@
+
+#! This program uses merge sort to sort an unserted array and prints it to console
+
+"""
+? In computer science, merge sort (also commonly spelled mergesort) is an efficient, general-purpose, comparison-based sorting algorithm. 
+? Most implementations produce a stable sort, which means that the order of equal elements is the same in the input and output. 
+? Merge sort is a divide and conquer algorithm that was invented by John von Neumann in 1945.[2] 
+? A detailed description and analysis of bottom-up merge sort appeared in a report by Goldstine and von Neumann as early as 1948.[3]
+
+? Conceptually, a merge sort works as follows:
+* Divide the unsorted list into n sublists, each containing one element (a list of one element is considered sorted).
+* Repeatedly merge sublists to produce new sorted sublists until there is only one sublist remaining. This will be the sorted list.
+"""
