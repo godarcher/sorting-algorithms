8b4e5f684c35c0a20f20761cec18a829b233b9e0
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 18:55:10 +0100

    implemented linear search

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -13,14 +13,18 @@
 """
 
 
-def linear_search(values, searched):
-    # The function takes in only two parameters.
-    # 'Searched' is the element being looked for.
-    # 'Values' is the list in which the element is searched.
-    for i in range(0, len(values)):
-        # We iterate over every element in the list.
-        if (values[i] == searched):
-            # If we find the element, we return the position of the element in the list.
-            return "Element " + str(searched) + " is at " + str(i+1)
-    # If we don't find the element, we return an 'absent statement'
-    return "Element has not been found"
+def linear_search(input_array, element_tofind):
+    # ? linear search checks all elements in a list till the value is found
+    # * it takes the input array and the element to be found
+
+    # For every element inside the input array
+    for i in range(0, len(input_array)):
+
+        # If it is the element we want to find
+        if (input_array[i] == element_tofind):
+
+            # Return the index of the found element.
+            return "Element " + str(element_tofind) + " is at index" + str(i+1)
+
+    # We tell the runner that the element has not been found if that's the case
+    return "Element is not inside the input array"
