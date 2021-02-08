00d2487bc168575e3ab9d27e175ddf095e0f027d
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 16:02:45 +0100

    added documentation for binary search

---
+++ binary_search.py
@@ -0,0 +1,13 @@
+
+#! This algorithm uses binary search to find an item inside a sorted database
+
+"""
+? In computer science, binary search, also known as half-interval search,[1] logarithmic search,[2] or binary chop,[3] 
+? is a search algorithm that finds the position of a target value within a sorted array.[4][5] 
+? Binary search compares the target value to the middle element of the array. If they are not equal, the half in which the target cannot lie is eliminated 
+? and the search continues on the remaining half, again taking the middle element to compare to the target value, and repeating this until the target value is found.
+? If the search ends with the remaining half being empty, the target is not in the array.
+
+* Binary search runs in logarithmic time in the worst case, making {\displaystyle O(\log n)}O(\log n) comparisons, 
+* where {\displaystyle n}n is the number of elements in the array.[a][6]
+"""
