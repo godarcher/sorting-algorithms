2121df93af09c60c12cb77a5d801c60dbc8e58e0
Joost Grunwald <joostgrunwald2001@gmail.com>
2021-02-08 16:47:02 +0100

    started implementation of linear search

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -11,3 +11,16 @@
 * Linear search is rarely practical because other search algorithms and schemes, such as the binary search algorithm and hash tables, 
 * allow significantly faster searching for all but short lists.[2]
 """
+
+
+def linear_search(values, searched):
+    # The function takes in only two parameters.
+    # 'Searched' is the element being looked for.
+    # 'Values' is the list in which the element is searched.
+    for i in range(0, len(values)):
+        # We iterate over every element in the list.
+        if (values[i] == searched):
+            # If we find the element, we return the position of the element in the list.
+            return "Element " + str(searched) + " is at " + str(i+1)
+    # If we don't find the element, we return an 'absent statement'
+    return "Element has not been found"
